ca30dbca80b06e590baf761a7b19bc3c33b47600
Bartlomiej Osinski <barower@gmail.com>
2018-12-03 01:12:19 +0100

    full screen with white color

--- main.c
+++ main.c
@@ -22,7 +22,20 @@ int main( int argc, char* args[] )
 		printf("Failed to init window: %s\n", SDL_GetError());
 	}
 
+	//Get window surface
+	screenSurface = SDL_GetWindowSurface(window);
+
+	//Fill the surface white
+	SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+
+	//Update the surface
+	SDL_UpdateWindowSurface(window);
+
 	SDL_Delay(2000);
+
+	SDL_DestroyWindow(window);
+
+	SDL_Quit();
 
 	return 0;
 }
