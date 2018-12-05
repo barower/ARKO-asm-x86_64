4db02e1742872cd17b96327fb81bb2868f6a2a58
Bartlomiej Osinski <barower@gmail.com>
2018-12-05 22:33:45 +0100

    drawGraph (C): added drawing with equal distance between pixels

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ CC = gcc
 
 COMPILER_FLAGS = -Wall -g
 
-LINKER_FLAGS =  -L/usr/lib/x86_64-linux-gnu -Wl,-rpath,/usr/local/lib -Wl,--enable-new-dtags -lSDL2 -lSDL2_ttf -D_REENTRANT -I/usr/include/SDL2
+LINKER_FLAGS =  -L/usr/lib/x86_64-linux-gnu -Wl,-rpath,/usr/local/lib -Wl,--enable-new-dtags -lSDL2 -lSDL2_ttf -lm -D_REENTRANT -I/usr/include/SDL2
 
 OBJ_NAME = arko_x86_64
 
--- drawgraph.c
+++ drawgraph.c
@@ -1,5 +1,6 @@
 #include "drawgraph.h"
 #include <stdio.h>
+#include <math.h>
 
 void drawGraph(SDL_Surface *surface, float *coefficients){
 	printf("Printing graph\n");
@@ -24,14 +25,24 @@ void drawGraph(SDL_Surface *surface, float *coefficients){
 	}
 
 	// Draw pixels
-	for(int i = 0; i < w; i++){
-		float x = -1.0 + i*2.0/w;
+	float x = -1.0;
+	unsigned int x_offset = 0;
+	while(x_offset < w){
+		// 1. Put pixel on screen
 		float y = A*x*x*x + B*x*x + C*x + D;
-		int y_offset = (int)((y + 1.0)*h/2.0);
-		int bufpos = i + y_offset*w;
+		unsigned int y_offset = (unsigned int)((y + 1.0)*h/2.0);
+		int bufpos = x_offset + y_offset*w;
 		if(bufpos > 0 && bufpos < w*h){
 			*(bufi + bufpos) = 0x00FF0000;
 		}
+
+		// 2. Calculate dx/dy
+		float derivative = 3*A*x*x + 2*B*x + C;
+
+		// 3. Calculate x step
+		float step = S / sqrt(1 + derivative*derivative);
+		x += step;
+		x_offset = (unsigned int)((x + 1.0)*w/2.0);
 	}
 
 }
