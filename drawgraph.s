31e663cd62c4db4e784fac0010b0647f8c6dc917
Bartlomiej Osinski <barower@gmail.com>
2018-12-09 00:22:56 +0100

    asm: first tries to see anything

--- drawgraph.s
+++ drawgraph.s
@@ -1,5 +1,12 @@
-    global      drawGraph_body
-    section     .text
+	global		drawGraph_body
+	section		.text
 
+;				rdi		      rsi	  rdx      rcx
+; void drawGraph_body(unsigned char *pPixelBuffer, int width, int height, int A,
+; int B, int C, int D, double S);
+;  r8     r9    stack    MMX0
 drawGraph_body:
-    ret
+	mov		[rdi], DWORD 0xFFFFFFFF
+	add		rdi, 4
+
+	ret
