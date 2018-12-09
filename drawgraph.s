2e7e846f60fb1d750ad98b629d309b0c6e517ad3
Bartlomiej Osinski <barower@gmail.com>
2018-12-09 01:09:40 +0100

    asm: Drawing y axis

--- drawgraph.s
+++ drawgraph.s
@@ -6,7 +6,23 @@
 ; int B, int C, int D, double S);
 ;  r8     r9    stack    MMX0
 drawGraph_body:
-	mov		[rdi], DWORD 0xFFFFFFFF
-	add		rdi, 4
+
+	;	Draw y axis
+	;		Prepare counter
+	mov		rax, rdx
+	;		*(buf + width/2 * 4), 4 is bytes per pixel
+	lea		r10, [rdi + rsi*2]
+	mov		[r10], DWORD 0xFFFFFFFF
+
+	;		Decrement counter
+	dec		rax
+	jz		xaxisinit
+yaxisloop:
+	lea		r10, [r10, rsi*4]
+	mov		[r10], DWORD 0xFFFFFFFF
+	dec		rax
+	jg		yaxisloop
+
+xaxisinit:
 
 	ret
